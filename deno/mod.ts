18522ac7d34180b3ee8ee8510cb1000aefe9b181
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2023-06-16 01:24:55 +0200

    Remove deno build

--- deno/mod.ts
+++
@@ -1,4 +0,0 @@
-export { createSanitize, sanitize } from './sanitize.ts';
-export { createRestore, restore } from './restore.ts';
-export { arrayType, dateType, specialNumberType, undefinedType, defaultTypes } from './types.ts';
-export type { CheckContext, CustomType, CustomTypes, RestoreContext, SanitizeContext } from './types.ts';
